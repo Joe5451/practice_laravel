c4f7d6e95a35bef88544a386d5956de6a9a69780
Joe <skybluejoe123@gmail.com>
2021-12-23 22:50:16 +0800

    add contact view

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
 
-<h1>Contact</h1>
+<div class="container py-5">
+    <h1 class="mb-5">Contact Us</h1>
+    <form action="" id="contact_form">
+        <div class="form-group">
+            <label>Name</label>
+            <input type="text" name="name" class="form-control">
+        </div>
+    
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="email" class="form-control">
+        </div>
+    
+        <div class="form-group">
+            <label>Message</label>
+            <textarea class="form-control" rows="3"></textarea>
+        </div>
+    
+        <button type="button" class="btn btn-primary mb-2">Submit</button>
+    </form>
+</div>
 
 @endsection
