f0802050950d3a4b72dc1ad009b2409147c9a24c
Joe <skybluejoe123@gmail.com>
2022-01-10 23:10:14 +0800

    'modify'

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LineBotController;
 
 // 在 heroku 路徑會變成 http，透過以下設定強制設為 https
-// URL::forceScheme('https');
+URL::forceScheme('https');
 
-Route::get('/line', [LineBotController::class, 'index'])->name('line');
+Route::post('/line', [LineBotController::class, 'index'])->name('line');
 
 Route::get('/', function () {
     return view('home');
